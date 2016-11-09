ee9ae0ef5288549941aa3efe48bfd9dcc81d932a
DavidMBP <steadypathapp@gmail.com>
2016-11-08 22:40:49 -0600

    Finishing off the UI view and the activity logic

--- ScribdWeather/app/src/main/java/com/steadypathapp/david/scribdweather/MainActivity.java
+++ ScribdWeather/app/src/main/java/com/steadypathapp/david/scribdweather/MainActivity.java
@@ -1,14 +1,76 @@
 package com.steadypathapp.david.scribdweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import com.steadypathapp.david.scribdweather.YahooAPI;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+import retrofit2.Response;
+
+
+public class MainActivity extends AppCompatActivity implements YahooNetworkTask.YahooNetworkTaskListener {
+
+    public final static String EXTRA_RESPONE = "com.steadypathapp.david.XML_REPONSE";
+
+    Button mButton;
+    EditText mEditCity;
+    TextView mError;
+
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button)findViewById(R.id.sub_button);
+        mEditCity = (EditText)findViewById(R.id.cityText);
+        mError = (TextView)findViewById(R.id.errorText);
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+    }
+
+
+    public void onSubmit(View view){
+        showProgress();
+        new YahooNetworkTask(this).execute(mEditCity.getText().toString());
+    }
+
+
+    @Override
+    public void onFinished(String city, Response<String> res) {
+
+        XMLExtractor xml = new XMLExtractor(res.body());
+        if(!xml.hasErrors()){
+            Intent intent = new Intent(this, WeatherActivity.class);
+            intent.putExtra(EXTRA_RESPONE, res.body());
+            startActivity(intent);
+        }else{
+
+            mError.setText("City Not Found");
+        }
+
+        hideProgress();
+
+    }
+
+
+    private void showProgress(){
+        mError.setText("");
+        mButton.setVisibility(View.INVISIBLE);
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgress(){
+        mButton.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.INVISIBLE);
     }
 }
--- ScribdWeather/app/src/main/java/com/steadypathapp/david/scribdweather/WeatherActivity.java
+++ ScribdWeather/app/src/main/java/com/steadypathapp/david/scribdweather/WeatherActivity.java
@@ -1,28 +1,46 @@
 package com.steadypathapp.david.scribdweather;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private XMLExtractor cityDoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Intent intent = getIntent();
+        String data = intent.getStringExtra(MainActivity.EXTRA_RESPONE);
+
+        cityDoc = new XMLExtractor(data);
+        getSupportActionBar().setTitle(cityDoc.getTitle());
+
+        CityWeather[] dataSet = cityDoc.getForecasts();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new ListAdapter(dataSet);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 }
